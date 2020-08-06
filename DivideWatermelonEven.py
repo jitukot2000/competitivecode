2448562ee5e5d48ceb769f082e9aa1b42d3fbf7b
jitukot2000 <35812692+jitukot2000@users.noreply.github.com>
2020-08-06 21:19:51 +0530

    Add files via upload

---
+++ DivideWatermelonEven.py
@@ -0,0 +1,9 @@
+####First Competitive Code challenge
+def DivideWatermelonEven(w):
+    if w % 2 == 0 and w >= 4:
+        print('YES')
+    else:
+        print('NO') 
+w = int(input(" "))
+
+DivideWatermelonEven(w)
